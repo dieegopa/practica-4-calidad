f18bb9c4340e5410e7e05a69e2ba6e94dfd5023f
dieegopa <dieegopa@localhost>
2021-12-28 22:16:24 -0500

    Commit de prueba

--- src/test/java/pruebas/TestApp.java
+++ src/test/java/pruebas/TestApp.java
@@ -1,21 +1,20 @@
-package ejercicio5;
+package pruebas;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class TestClass extends TestCase{
-	
-	public TestClass(String testName) {
+public class TestApp extends TestCase{
+
+	public TestApp(String testName) {
 		super(testName);
 	}
 	
 	public static Test suite() {
-		return new TestSuite(TestClass.class);
+		return new TestSuite(TestApp.class);
 	}
 	
 	public void testApp() {
 		assertTrue(false);
 	}
-
 }
